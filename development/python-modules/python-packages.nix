50e82439de4bc6ba34becb918e1a03d3fabc7292
Anders Riutta <git@andersriutta.com>
2019-09-05 18:02:08 -0700

    flask-mwoauth: added

--- development/python-modules/python-packages.nix
+++ development/python-modules/python-packages.nix
@@ -3,4 +3,8 @@ with import <nixpkgs> { config.allowUnfree = true; };
 {
   arviz = callPackage ./arviz/default.nix {};
   daff = (callPackage ./daff/requirements.nix {}).packages.daff;
+  flask-mwoauth = callPackage ./flask-mwoauth/default.nix {};
+#  flask-mwoauth = (
+#    callPackage ./flask-mwoauth/requirements.nix {}
+#  ).packages.flask-mwoauth;
 }
