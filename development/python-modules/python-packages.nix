fa86035e4d896e83f61ad4d4dcfdbcdcdc782e3a
Anders Riutta <git@andersriutta.com>
2019-09-10 10:34:26 -0700

    Clean out dead code

--- development/python-modules/python-packages.nix
+++ development/python-modules/python-packages.nix
@@ -4,7 +4,4 @@ with import <nixpkgs> { config.allowUnfree = true; };
   arviz = callPackage ./arviz/default.nix {};
   daff = (callPackage ./daff/requirements.nix {}).packages.daff;
   flask-mwoauth = callPackage ./flask-mwoauth/default.nix {};
-#  flask-mwoauth = (
-#    callPackage ./flask-mwoauth/requirements.nix {}
-#  ).packages.flask-mwoauth;
 }
