fbe97537c079afd848fc58fbb3262b79d7691003
Anders Riutta <git@andersriutta.com>
2021-02-09 15:27:43 -0800

    black: remove b/c nixpkgs has it now

--- all-custom.nix
+++ all-custom.nix
@@ -8,7 +8,6 @@ mergeAttrs nodePackages {
   python3Packages = python3Packages;
 
   bash-it = callPackage ./bash-it/default.nix {}; 
-  black = callPackage ./black/default.nix {}; 
   composer2nix = import (fetchTarball https://api.github.com/repos/svanderburg/composer2nix/tarball/8453940d79a45ab3e11f36720b878554fe31489f) {}; 
   java-buildpack-memory-calculator = callPackage ./java-buildpack-memory-calculator/default.nix {};
   jupyterlab-connect = callPackage ./jupyterlab-connect/default.nix {};
